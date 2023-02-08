6a269a8b0c3b03571e9b3b7ead2a23322b1d5a77
Mohammadzaki0027 <zaki7875@gmail.com>
2023-02-08 11:04:50 +0530

    the clearing part

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-  const  backendurl=`http://localhost:8080`
-   export default backendurl;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
